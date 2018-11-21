3ffff09155378d82edcc8d231cf3b1dd8c39798e
KC2A3 <947496351@qq.com>
2018-11-21 22:05:55 +0800

    Use Sublime to optimize code format

--- js/firstUppercase.js
+++ js/firstUppercase.js
@@ -1,12 +1,13 @@
 var sentence = 'good afternoon, mr mike.';
+
 function convertFirstLetterUppercaseForEveryWord() {
-    let words = sentence.split(' ');
-    for(var i = 0 ; i < words .length ; i++){
-        let uppercaseFirstLetter = words[i].slice(0,1).toUpperCase();
-        let otherLetters = words[i].slice(1);
-        words[i]=uppercaseFirstLetter+otherLetters;
-    }
-    let resultSentence = words.join(' ');
-    return resultSentence;
+  let words = sentence.split(' ');
+  for (var i = 0; i < words.length; i++) {
+    let uppercaseFirstLetter = words[i].slice(0, 1).toUpperCase();
+    let otherLetters = words[i].slice(1);
+    words[i] = uppercaseFirstLetter + otherLetters;
+  }
+  let resultSentence = words.join(' ');
+  return resultSentence;
 }
 console.log(convertFirstLetterUppercaseForEveryWord(sentence));
